7c9bd5c3313d94fc2dc0c2fffc2fcafd59058c5a
Ab_Kafi <58607642+abkafi1234@users.noreply.github.com>
2026-05-15 15:03:47 +0600

    Update version to 0.1.1 in __init__.py

--- src/normeval/__init__.py
+++ src/normeval/__init__.py
@@ -2,7 +2,7 @@
 from .evaluator import NormalizationEvaluator
 
 # Define the version in one place
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 # Explicitly define what is exported
 __all__ = ["NormalizationEvaluator"]
